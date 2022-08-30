88969144a867c73fdee00ae439349ab2accfc3b8
Nikhil Sinha <nikhilsinha198@gmail.com>
2022-08-30 13:45:19 +0530

    Day1: data-types and variable n constant

---
+++ Day_1_JS/Day_1.js
@@ -0,0 +1,31 @@
+console.log('Day 1 of 30 days of Revision set of JS');
+
+// DAy_1: Today we are gonna revise about js variable and data types
+// in js there are 2 ways of declaring(after 2016) variable
+
+let a = 10; // Number
+let b = 'Nikhil'; // String
+let c = true; // Boolean
+let d = 10.32; // Number
+let arr = [12, 3, 'String']; // Object
+let e; // Undefined
+
+// Objects
+const aboutMe = {
+	//  key: value
+	name: 'Niikhil',
+	age: 22,
+	profesion: 'Student',
+	address: 'New Delhi'
+};
+
+// let is use to assign variable name
+// and const is used to assign a constant name
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof arr);
+console.log(e);
+console.log(typeof aboutMe);
